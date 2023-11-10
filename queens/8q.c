b0b37bc52bfee1d5fd62088fd34425c395d1b7f4
Ali Jardine <jardine64@gmail.com>
2023-11-10 16:03:51 +0000

    still working with pointers.

--- queens/8q.c
+++ queens/8q.c
@@ -19,7 +19,7 @@ int main(int argc, char * argv[])
     boards[start_index]=create_empty_board(size);
     
     while (start_index<end_index){
-        end_index = add_child_boards(boards[start_index], boards, end_index, &num_solutions, is_verbose);
+        end_index = add_child_boards(&boards[start_index], boards, end_index, &num_solutions, is_verbose);
         start_index++;
     }
 
@@ -63,56 +63,40 @@ void prompt_correct_usage(void)
     exit(EXIT_FAILURE);
 }
 
-//    Board f = create_empty_board(8);
-//    num_index = 0;
-//    num_solutions=0;
-//    add_child_boards(f, boards, next_index, &num_solutions);
-//    assert(num_solutions==92)
-
-/* Adds all possible next 
-child boards from the specified board b
- to the given array.
- Returns the size of the array (ie the next_index)
- Also checks if a board is a solution and modifies that number.
-  */
-int add_child_boards(Board b, Board boards[BOARDS_IN_LIST], int next_index, int * num_solutions, bool is_verbose)
+int add_child_boards(Board * b, Board boards[BOARDS_IN_LIST], int next_index, int * num_solutions, bool is_verbose)
 {
-    for (int r=0; r<b.size; r++){
-        for (int c=0; c<b.size; c++){
-            // TODO: move this into own func to avoid nested ifs
-            if(can_place_queen(r,c,b)){
-                Board temp = copy_board(b);
-                temp.grid[r][c]='Q';
-                temp.num_queens++;
-                if (board_is_unique(temp, boards, next_index)){
-                    boards[next_index] = temp;
-                    next_index++;
-                    if(is_solved_board(temp)){
-                        if(is_verbose){
-                            print_board_string(temp);
-                        }
-                        (*num_solutions)++;
-                    }                  
-                }
-            }
+    for (int r=0; r<b->size; r++){
+        for (int c=0; c<b->size; c++){
+           next_index = add_child_board(b, boards, next_index, num_solutions, is_verbose, r, c);
         }
     }
     return next_index;
 }
 
-// TODO: potentially createa a create_child_board funct.
-// This should do the logic bit from add child board, and determine
-// if you should add the board you just made (temp) to the array or not.
-// (ie can you place queen, is it unique.)
+int add_child_board(Board * b, Board boards[BOARDS_IN_LIST], int next_index, int * num_solutions, bool is_verbose, int r, int c)
+{
+    if(can_place_queen(r, c, b)){
+        Board temp = copy_board(*b);
+        temp.grid[r][c]='Q';
+        temp.num_queens++;
+        if (board_is_unique(&temp, boards, next_index)){
+            boards[next_index] = temp;
+            next_index++;
+            if(is_solved_board(temp)){
+                if(is_verbose){
+                    print_board_string(&temp);
+                }
+                (*num_solutions)++;
+            }                  
+        }
+    }
+    return next_index;
+}
 
-// TODO: you might want to create funcs that return boards for testing. 
-// eg: create solved board_8
-// this way you don't need to have the same strings in different tests? Ask Neil.
-
-bool board_is_unique(Board a, Board boards[BOARDS_IN_LIST], int size)
+bool board_is_unique(Board * a, Board boards[BOARDS_IN_LIST], int size)
 {
     for (int i=0; i<size; i++){
-        if(are_boards_identical(a, boards[i])){
+        if(are_boards_identical(a, &boards[i])){
             return false;
         }
     }
@@ -137,10 +121,10 @@ void test_board_is_unique(void)
     boards[0] = c;
     // since you've already added this board
     // should be false.
-    assert(board_is_unique(c, boards, 1)==false);
+    assert(board_is_unique(&c, boards, 1)==false);
     strcpy(c.grid[7], "--------");
     // now we've modified c to be a different board and test again.
-    assert(board_is_unique(c, boards, 1));
+    assert(board_is_unique(&c, boards, 1));
     // TODO: ideally we'd add more to this function.
 }
 
@@ -171,20 +155,9 @@ void test_is_solved_board(void)
     assert(is_solved_board(c)==false);
 }
 
-
-// TODO: check if this is the best way to do this?
-Board copy_board(Board source)
+Board copy_board(Board b)
 {
-    Board dest;
-    dest.size = source.size;
-    dest.num_queens = source.num_queens;
-
-    for (int r=0; r<source.size; r++){
-        for (int c=0; c<source.size; c++){
-            dest.grid[r][c]=source.grid[r][c];
-        }
-    }
-    return dest;
+    return b;
 }
 
 void print_board(Board b)
@@ -219,7 +192,7 @@ void test_add_child_boards(void)
     strcpy(c.grid[7], "--Q-----");
     int next_index = 0;
     int num_solutions = 0;
-    assert(add_child_boards(c, boards, next_index, &num_solutions, false)==0);
+    assert(add_child_boards(&c, boards, next_index, &num_solutions, false)==0);
     assert(num_solutions==0);
 
     // board with only one child board.
@@ -236,7 +209,7 @@ void test_add_child_boards(void)
     strcpy(d.grid[7], "--------");
     next_index = 0;
     num_solutions=0;
-    next_index = add_child_boards(d, boards, next_index, &num_solutions, false);
+    next_index = add_child_boards(&d, boards, next_index, &num_solutions, false);
     assert(next_index==1);
     assert(boards[0].grid[7][2]=='Q');
     assert(num_solutions==1);
@@ -247,7 +220,7 @@ void test_add_child_boards(void)
     Board e = create_empty_board(3);
     next_index = 0;
     num_solutions=0;
-    next_index = add_child_boards(e, boards, next_index, &num_solutions, false);
+    next_index = add_child_boards(&e, boards, next_index, &num_solutions, false);
     assert(next_index==9);
     assert(num_solutions==0);
     
@@ -255,13 +228,13 @@ void test_add_child_boards(void)
 
 /* Returns if it's possible to place a queen at the given
 coordinates. */
-bool can_place_queen(int r, int c, Board b)
+bool can_place_queen(int r, int c, Board * b)
 {
   if (!is_valid_cord(r, c, b)){
     return false;
   }
   // queen in square already.
-  if (b.grid[r][c] == 'Q'){
+  if (b->grid[r][c] == 'Q'){
     return false;
   }
   if (queen_in_row(r, b)){
@@ -278,10 +251,10 @@ bool can_place_queen(int r, int c, Board b)
 
 // TODO. you have a lot of functions that copy structs. 
 // Using pointers might make all of them more efficient.
-bool queen_in_row(int r, Board b)
+bool queen_in_row(int r, Board * b)
 {
-    for (int i=0; i<b.size; i++){
-        if(b.grid[r][i]=='Q'){
+    for (int i=0; i<b->size; i++){
+        if(b->grid[r][i]=='Q'){
             return true;
         }
     }
@@ -301,17 +274,17 @@ void test_queen_in_row(void)
     strcpy(b.grid[5], "--------");
     strcpy(b.grid[6], "--------");
     strcpy(b.grid[7], "--------");
-    assert(queen_in_row(0,b));
-    assert(queen_in_row(2,b));
-    assert(queen_in_row(3,b));
-    assert(queen_in_row(1,b)==false);
+    assert(queen_in_row(0,&b));
+    assert(queen_in_row(2,&b));
+    assert(queen_in_row(3,&b));
+    assert(queen_in_row(1,&b)==false);
 
 }
 
-bool queen_in_col(int c, Board b)
+bool queen_in_col(int c, Board * b)
 {
-    for (int i=0; i<b.size; i++){
-        if(b.grid[i][c]=='Q'){
+    for (int i=0; i<b->size; i++){
+        if(b->grid[i][c]=='Q'){
             return true;
         }
     }
@@ -331,15 +304,15 @@ void test_queen_in_col(void)
     strcpy(b.grid[5], "--------");
     strcpy(b.grid[6], "--------");
     strcpy(b.grid[7], "-------Q");
-    assert(queen_in_col(0,b)==false);
-    assert(queen_in_col(1,b));
-    assert(queen_in_col(3,b));
-    assert(queen_in_col(4,b)==false);
-    assert(queen_in_col(7,b));
+    assert(queen_in_col(0,&b)==false);
+    assert(queen_in_col(1,&b));
+    assert(queen_in_col(3,&b));
+    assert(queen_in_col(4,&b)==false);
+    assert(queen_in_col(7,&b));
 }
 
 // TODO: this is too long. Split it up.
-bool queen_in_diagonals(int r, int c, Board b)
+bool queen_in_diagonals(int r, int c, Board * b)
 {
     // up and left
     // TODO: if you wanted you could start at r-1 and c-1
@@ -347,7 +320,7 @@ bool queen_in_diagonals(int r, int c, Board b)
     int j = r;
     int i = c;
     while (is_valid_cord(j, i, b)){
-        if(b.grid[j][i]=='Q'){
+        if(b->grid[j][i]=='Q'){
             return true;
         }
         j--;
@@ -358,7 +331,7 @@ bool queen_in_diagonals(int r, int c, Board b)
     j = r;
     i = c;
     while (is_valid_cord(j, i, b)){
-        if(b.grid[j][i]=='Q'){
+        if(b->grid[j][i]=='Q'){
             return true;
         }
         j--;
@@ -369,7 +342,7 @@ bool queen_in_diagonals(int r, int c, Board b)
     j = r;
     i = c;
     while (is_valid_cord(j, i, b)){
-        if(b.grid[j][i]=='Q'){
+        if(b->grid[j][i]=='Q'){
             return true;
         }
         j++;
@@ -380,7 +353,7 @@ bool queen_in_diagonals(int r, int c, Board b)
     j = r;
     i = c;
     while (is_valid_cord(j, i, b)){
-        if(b.grid[j][i]=='Q'){
+        if(b->grid[j][i]=='Q'){
             return true;
         }
         j++;
@@ -402,11 +375,11 @@ void test_queen_in_diagonals(void)
     strcpy(b.grid[5], "--------");
     strcpy(b.grid[6], "--------");
     strcpy(b.grid[7], "--------");
-    assert(queen_in_diagonals(0,0,b)); //down and right
-    assert(queen_in_diagonals(4,0,b)); // up and right
-    assert(queen_in_diagonals(0,4,b)); // down and left 
-    assert(queen_in_diagonals(4,4,b)); // up and left
-    assert(queen_in_diagonals(3,2,b)==false);
+    assert(queen_in_diagonals(0,0,&b)); //down and right
+    assert(queen_in_diagonals(4,0,&b)); // up and right
+    assert(queen_in_diagonals(0,4,&b)); // down and left 
+    assert(queen_in_diagonals(4,4,&b)); // up and left
+    assert(queen_in_diagonals(3,2,&b)==false);
 }
 
 void test_is_valid_cord(void)
@@ -414,32 +387,32 @@ void test_is_valid_cord(void)
     Board b;
     b.num_queens = 8;
     b.size = 8;
-    assert(is_valid_cord(0,8,b)==false); // off the board
-    assert(is_valid_cord(0,0,b));
-    assert(is_valid_cord(7,0,b));
-    assert(is_valid_cord(5,2,b));
+    assert(is_valid_cord(0,8,&b)==false); // off the board
+    assert(is_valid_cord(0,0,&b));
+    assert(is_valid_cord(7,0,&b));
+    assert(is_valid_cord(5,2,&b));
 
 }
 
-bool is_valid_cord(int r, int c, Board b)
+bool is_valid_cord(int r, int c, Board * b)
 {
-    if (r>=0 && c>=0 && r<b.size && c<b.size){
+    if (r>=0 && c>=0 && r<b->size && c<b->size){
         return true;
     }
     return false;
 }
 
-bool are_boards_identical(Board b, Board c)
+bool are_boards_identical(Board * b, Board * c)
 {
-    if (b.num_queens!=c.num_queens){
+    if (b->num_queens!=c->num_queens){
         return false;
     }
-     if (b.size!=c.size){
+     if (b->size!=c->size){
         return false;
     }
-    for (int j=0; j<b.size; j++){
-        for (int i=0; i<b.size; i++){
-            if(b.grid[j][i]!=c.grid[j][i]){
+    for (int j=0; j<b->size; j++){
+        for (int i=0; i<b->size; i++){
+            if(b->grid[j][i]!=c->grid[j][i]){
                 return false;
             }
         }
@@ -449,14 +422,14 @@ bool are_boards_identical(Board b, Board c)
 
 // TODO create a testable version?
 // also this prints a new line at the end.
-void print_board_string(Board b)
+void print_board_string(Board * b)
 {
     // loop through every row in each collumns. 
     // If a queen is present print the row.
-    for (int c=0; c<b.size; c++){
-        for (int r=0; r<b.size; r++){
-            if(b.grid[r][c]=='Q'){
-                printf("%i",b.size-r);
+    for (int c=0; c<b->size; c++){
+        for (int r=0; r<b->size; r++){
+            if(b->grid[r][c]=='Q'){
+                printf("%i",b->size-r);
             }
         }
     }
@@ -478,7 +451,7 @@ void test_are_boards_indentical(void)
     strcpy(example8.grid[7], "----------");
     strcpy(example8.grid[8], "----------");
     strcpy(example8.grid[9], "----------");
-    assert(are_boards_identical(example8,example8)); 
+    assert(are_boards_identical(&example8, &example8)); 
 
     Board example8_2;
     example8_2.num_queens =8;
@@ -492,7 +465,7 @@ void test_are_boards_indentical(void)
     strcpy(example8_2.grid[6], "-----Q--");
     strcpy(example8_2.grid[7], "--Q-----");
 
-    assert(are_boards_identical(example8,example8_2)==false); 
+    assert(are_boards_identical(&example8, &example8_2)==false); 
 }
 
 Board create_empty_board(int size)
@@ -526,7 +499,7 @@ void test_create_empty_board(void)
     strcpy(example8.grid[8], "----------");
     strcpy(example8.grid[9], "----------");
 
-    are_boards_identical(b, example8);
+    assert(are_boards_identical(&b, &example8));
 
 }
 
@@ -544,14 +517,14 @@ void test_can_place_queen(void)
     strcpy(b.grid[6], "--------");
     strcpy(b.grid[7], "--------");
     
-    assert(can_place_queen(1,7,b)); // nothing in row, col, or diagonal
-    assert(can_place_queen(6,3,b)); // nothing in row, col, or diagonal
-    assert(can_place_queen(0,8,b)==false); // off the board
-    assert(can_place_queen(2,0,b)==false); // off the board
-    assert(can_place_queen(0,0,b)==false); // Q already there.
-    assert(can_place_queen(0,6,b)==false); // Q in col (right above)
-    assert(can_place_queen(1,0,b)==false); // Q in this col.
-    assert(can_place_queen(2,0,b)==false); // Q in this row.
+    assert(can_place_queen(1,7,&b)); // nothing in row, col, or diagonal
+    assert(can_place_queen(6,3,&b)); // nothing in row, col, or diagonal
+    assert(can_place_queen(0,8,&b)==false); // off the board
+    assert(can_place_queen(2,0,&b)==false); // off the board
+    assert(can_place_queen(0,0,&b)==false); // Q already there.
+    assert(can_place_queen(0,6,&b)==false); // Q in col (right above)
+    assert(can_place_queen(1,0,&b)==false); // Q in this col.
+    assert(can_place_queen(2,0,&b)==false); // Q in this row.
 
     Board c;
     c.num_queens = 8;
@@ -566,15 +539,15 @@ void test_can_place_queen(void)
     strcpy(c.grid[7], "--Q-----");
     // this board is already solved, 
     // so whereever you put a queen it should be false.
-    assert(can_place_queen(0,0,c)==false);
-    assert(can_place_queen(3,0,c)==false);
-    assert(can_place_queen(8,0,c)==false);
-    assert(can_place_queen(0,2,c)==false);
-    assert(can_place_queen(3,1,c)==false);
-    assert(can_place_queen(4,2,c)==false);
-    assert(can_place_queen(7,4,c)==false);
-    assert(can_place_queen(1,1,c)==false);
-    assert(can_place_queen(4,3,c)==false);
+    assert(can_place_queen(0,0,&c)==false);
+    assert(can_place_queen(3,0,&c)==false);
+    assert(can_place_queen(8,0,&c)==false);
+    assert(can_place_queen(0,2,&c)==false);
+    assert(can_place_queen(3,1,&c)==false);
+    assert(can_place_queen(4,2,&c)==false);
+    assert(can_place_queen(7,4,&c)==false);
+    assert(can_place_queen(1,1,&c)==false);
+    assert(can_place_queen(4,3,&c)==false);
 
     }
 
--- queens/8q.h
+++ queens/8q.h
@@ -17,17 +17,17 @@ typedef struct Board{
 void test(void);
 
 void test_are_boards_indentical(void);
-bool are_boards_identical(Board b, Board c);
+bool are_boards_identical(Board * b, Board * c);
 
 void parse_args(int argc, char * argv[], int * size, bool * is_vebose);
 
 int convert_and_verify_size(char * size_str);
 
-void print_board_string(Board b);
+void print_board_string(Board * b);
 
 void print_board(Board b);
 
-Board copy_board(Board source);
+Board copy_board(Board b);
 
 bool is_solved_board(Board b);
 
@@ -36,24 +36,25 @@ void prompt_correct_usage(void);
 Board create_empty_board(int size);
 void test_create_empty_board(void);
 
-bool board_is_unique(Board a, Board boards[BOARDS_IN_LIST], int size);
+bool board_is_unique(Board * a, Board boards[BOARDS_IN_LIST], int size);
 void test_board_is_unique(void);
 
-bool is_valid_cord(int r, int c, Board b);
+bool is_valid_cord(int r, int c, Board * b);
 void test_is_valid_cord(void);
 
-bool queen_in_row(int r, Board b);
+bool queen_in_row(int r, Board * b);
 void test_queen_in_row(void);
 
-bool queen_in_col(int c, Board b);
+bool queen_in_col(int c, Board * b);
 void test_queen_in_col(void);
 
-bool queen_in_diagonals(int r, int c, Board b);
+bool queen_in_diagonals(int r, int c, Board * b);
 void test_queen_in_diagonals(void);
 
-bool can_place_queen (int r, int c, Board b);
+bool can_place_queen (int r, int c, Board * b);
 void test_can_place_queen(void);
 
-int add_child_boards(Board b, Board boards[BOARDS_IN_LIST], int next_index, int * num_solutions, bool is_verbose);
+int add_child_boards(Board * b, Board boards[BOARDS_IN_LIST], int next_index, int * num_solutions, bool is_verbose);
 void test_add_child_boards(void);
 
+int add_child_board(Board * b, Board boards[BOARDS_IN_LIST], int next_index, int * num_solutions, bool is_verbose, int r, int c);
