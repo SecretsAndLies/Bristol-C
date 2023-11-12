a3628fe5fd067186b9a62f4dd25a331afa900f34
Ali Jardine <jardine64@gmail.com>
2023-11-12 10:45:23 +0000

    adding neils SDL files as starter code.

--- queens/extension.c
+++ queens/extension.c
@@ -1,11 +1,54 @@
-#include <SDL.h>
 
 // NEIL HAS DEMO CODE:
 // https://github.com/csnwc/Exercises-In-C/blob/f3803259aa6a0bfc210d6bf07c8d7e578bde9333/Code/Week5/SDL/demo_neillsdl2.c
 
-int main(void){
-  /* initialize SDL */
-  SDL_Init(SDL_INIT_VIDEO);
+#include <stdio.h>
+#include <stdlib.h>
+#include "neillsdl2.h"
 
+#define RECTSIZE 20
+#define MILLISECONDDELAY 20
+
+int main(void)
+{
+
+   SDL_Simplewin sw;
+   SDL_Rect rectangle;
+   rectangle.w = RECTSIZE;
+   rectangle.h = RECTSIZE;
+
+   Neill_SDL_Init(&sw);
+   do{
+
+      SDL_Delay(MILLISECONDDELAY);
+
+      /* Choose a random colour, a mixture of red, green and blue. */
+      Neill_SDL_SetDrawColour(&sw, rand()%SDL_8BITCOLOUR,
+                                   rand()%SDL_8BITCOLOUR,
+                                   rand()%SDL_8BITCOLOUR);
+
+      /* Filled Rectangle, fixed size, random position */
+      rectangle.x = rand()%(WWIDTH-RECTSIZE);
+      rectangle.y = rand()%(WHEIGHT-RECTSIZE);
+      SDL_RenderFillRect(sw.renderer, &rectangle);
+
+      /* Unfilled Rectangle, fixed size, random position */
+      rectangle.x = rand()%(WWIDTH-RECTSIZE);
+      rectangle.y = rand()%(WHEIGHT-RECTSIZE);
+      SDL_RenderDrawRect(sw.renderer, &rectangle);
+
+      Neill_SDL_UpdateScreen(&sw);
+
+      /* Has anyone pressed ESC or killed the SDL window ?
+         Must be called frequently - it's the only way of escaping  */
+      Neill_SDL_Events(&sw);
+
+   }while(!sw.finished);
+
+   /* Clear up graphics subsystems */
+   SDL_Quit();
+   atexit(SDL_Quit);
+
+   return 0;
 
 }
